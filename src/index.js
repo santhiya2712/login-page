a0104bfbcdd59065915cc6068a370d56ed85047e
santhiya2712 <santhiya2712@gmail.com>
2021-11-01 14:28:56 +0530

    to add a image

--- src/index.js
+++ src/index.js
@@ -6,9 +6,13 @@ import styles from "./styles/login.module.css";
 const Loginpage = () =>{
   return (
     <div className={styles.container}>
+      <div className={styles.images}>
+        <img src="image_1.png" alt=" "/>
+      </div>
+      
+      <div className={styles.loginform}>
       <p>Welcome back</p>
       <h2>Login to your account</h2>
-      <div className="loginform">
         <form>
           <p>Email</p>
           <input type="email" placeholder="John.snow@gmail.com"/>
@@ -21,7 +25,7 @@ const Loginpage = () =>{
         <p style={{color:"#2C5282"}}>Forgot password?</p>
       </div>
       <button className={styles.loginbutton}>login now</button><br/>
-      <button className={styles.Googlebutton}>sign-in with google</button>  
+      <button className={styles.Googlebutton}><img src="iconfinderGoogle.png" alt=" "/>or sign-in with google</button>  
       </div>
     </div>
   );
