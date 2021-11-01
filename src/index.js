4c18a6d52f75e607e01c4177da3a98c5d66f9812
santhiya2712 <santhiya2712@gmail.com>
2021-11-01 11:52:32 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
+import React  from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import styles from "./styles/login.module.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Loginpage = () =>{
+  return (
+    <div className={styles.container}>
+      <p>Welcome back</p>
+      <h2>Login to your account</h2>
+      <div className="loginform">
+        <form>
+          <p>Email</p>
+          <input type="email" placeholder="John.snow@gmail.com"/>
+          <p>Password</p>
+          <input type="password" placeholder="*********"/>
+        </form>
+        <div className={styles.pwdremaining}>
+        <div><input type="radio"/>
+        <label>Remeber me</label></div>   
+        <p style={{color:"#2C5282"}}>Forgot password?</p>
+      </div>
+      <button className={styles.loginbutton}>login now</button><br/>
+      <button className={styles.Googlebutton}>sign-in with google</button>  
+      </div>
+    </div>
+  );
+};
+ReactDOM.render(<Loginpage />, document.getElementById('root'));
